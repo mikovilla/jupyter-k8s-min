86a06606db74c2d83a7d0892cb59e9345b86dcf0
Miko Villa <dev@mikovilla.com>
2025-08-23 06:11:52 +0800

    add mem and cpus

--- minikube-docker.sh
+++ minikube-docker.sh
@@ -1,9 +1,14 @@
-PROFILE=jupyter-k8s-min
+PROFILE=MyNotebooks
+MEMORY=16384
+CPUS=4
 
 minikube delete -p "$PROFILE" || true
 
 minikube start -p "$PROFILE" \
+  --memory=$MEMORY \
+  --cpus=$CPUS \
   --driver=docker \
   --mount --mount-string="$HOME/projects/$PROFILE/drive:/drive"
 
 eval "$(minikube -p "$PROFILE" docker-env)"
+
